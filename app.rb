a3749d36815cc7616f472ffcba6a25cd706e294e
Jared Grove <jaredkgrove@gmail.com>
2019-07-08 16:37:27 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -13,7 +13,12 @@ class App < Sinatra::Base
   end
 
   get "/say/:number/:phrase" do
+    @number = params[:number]
+    @phrase = params[:phrase]
 
+    @number.to_i.times do 
+      "#{@phrase}"
+    end
   end
 
   get "/say/:word1/:word2/:word3/:word4/:word5" do
