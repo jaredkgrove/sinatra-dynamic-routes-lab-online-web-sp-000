1980a4d04bd993f86d61c1cbbb6dc0661098e655
Jared Grove <jaredkgrove@gmail.com>
2019-07-08 16:34:08 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -8,15 +8,15 @@ class App < Sinatra::Base
   end
 
   get "/square/:number" do
-    
+
   end
 
   get "/say/:number/:phrase" do
-    
+
   end
 
   get "/say/:word1/:word2/:word3/:word4/:word5" do
-    
+
   end
 
   get "/:operation/:number1/:number2"
