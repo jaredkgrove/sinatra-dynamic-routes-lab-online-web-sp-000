02f77edb446fc77f2d22cee38a7490eb8d449568
Jared Grove <jaredkgrove@gmail.com>
2019-07-08 16:49:18 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -36,9 +36,13 @@ class App < Sinatra::Base
 
     case @operation
     when "add"
+      "#{@number1 + @number2}"
     when "subtract"
+      "#{@number1 - @number2}"
     when "divide"
+      "#{@number1 / @number2}"
     when "multiply"
+      "#{@number1 * @number2}"
     end
   end
 
